5fcd1a8bfe6ae5a8c6429f97f99906c888f574b7
Paul Madden <paul.madden@noaa.gov>
2026-09-22 15:26:42 +0000

    WIP

--- workflow.py
+++ workflow.py
@@ -9,15 +9,15 @@ from uwtools.api.utils import run_shell_cmd
 DIR = Path("/run/aigfs")
 CMD = f"podman run -v .:{DIR} ghcr.io/maddenp-cu/aigfs:latest run cmd"
 
-type C = datetime | str
+type CycleT = datetime | str
 
 # Public tasks:
 
 
 @task
-def config(c: C) -> Iterator:
-    c = _dt(c)
-    name = "Cycle %s config" % c
+def config(cycle_: CycleT) -> Iterator:
+    cycle_ = _dt(cycle_)
+    name = "Cycle %s config" % cycle_
     yield name
     path = Path(DIR / "aigfs.yaml")
     yield Asset(path, path.is_file)
@@ -30,46 +30,47 @@ def config(c: C) -> Iterator:
 
 @collection
 def cycle(cyclestr: str) -> Iterator:
-    c = _dt(cyclestr)
-    yield "Cycle %s" % _dt(c)
+    cycle_ = _dt(cyclestr)
+    yield "Cycle %s" % _dt(cycle_)
     yield post()
 
 
 @task
-def forecast(c: C) -> Iterator:
-    c = _dt(c)
-    yield "Cycle %s forecast" % c
+def forecast(cycle_: CycleT) -> Iterator:
+    cycle_ = _dt(cycle_)
+    yield "Cycle %s forecast" % cycle_
     path = Path("forecast")
     Asset(path, path.is_file)
-    yield prep(c)
+    yield prep(cycle_)
     path.touch()
 
 
 @task
-def post(c: C) -> Iterator:
-    c = _dt(c)
-    yield "Cycle %s post" % c
+def post(cycle_: CycleT) -> Iterator:
+    cycle_ = _dt(cycle_)
+    yield "Cycle %s post" % cycle_
     path = Path("post")
     yield Asset(path, path.is_file)
-    yield forecast(c)
+    yield forecast(cycle_)
     path.touch()
 
 
 @task
-def prep(c: C) -> Iterator:
-    c = _dt(c)
-    name = "Cycle %s prep" % c
+def prep(cycle_: CycleT) -> Iterator:
+    cycle_ = _dt(cycle_)
+    name = "Cycle %s prep" % cycle_
     yield name
     path = Path("prep/aigfs.t00z.ic.nc")
     yield Asset(path, path.is_file)
-    yield [_timegate(c), config(c)]
+    config_ = config(cycle_)
+    yield [_timegate(cycle_), config_]
     cmd = [
         f"{CMD} uw execute",
         "--module aigfs.drivers.ics",
         "--classname AIGFSICs",
         "--task run",
-        f"--config {DIR}/aigfs.yaml",
-        "--cycle %s" % c.isoformat(),
+        f"--config {config_.ref}",
+        "--cycle %s" % cycle_.isoformat(),
         "--key-path prep",
     ]
     run_shell_cmd(" ".join(cmd), taskname=name)
@@ -79,9 +80,9 @@ def prep(c: C) -> Iterator:
 
 
 @external
-def _timegate(c: datetime) -> Iterator:
+def _timegate(cycle_: datetime) -> Iterator:
     offset = timedelta(hours=3, minutes=35)
-    cutoff = c + offset
+    cutoff = cycle_ + offset
     yield from [
         "UTC > %s" % cutoff.replace(tzinfo=None),
         Asset(None, lambda: datetime.now(UTC) > cutoff),
@@ -91,7 +92,7 @@ def _timegate(c: datetime) -> Iterator:
 # Private helpers:
 
 
-def _dt(c: C) -> datetime:
-    if isinstance(c, str):
-        return datetime.fromisoformat(c).replace(tzinfo=timezone.utc)
-    return c
+def _dt(cycle_: CycleT) -> datetime:
+    if isinstance(cycle_, str):
+        return datetime.fromisoformat(cycle_).replace(tzinfo=timezone.utc)
+    return cycle_
